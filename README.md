e5374212afec20ea98104f864280a43645b3d9ea
Adeeetcoeg <coegpengencuan@gmail.com>
2022-03-29 22:33:22 +0700

    KEGABUTAN YANG BER FAEDAH PREN WKWK

---
+++ README.md
@@ -0,0 +1,6 @@
+# mario
+
+Mario game using Flutter.
+
+Coeg gabut di malam hari pren wkwkw
+dari pada ot mnding belajar hal baru hehehe
---
+++ android/app/src/main/kotlin/com/example/gamexco/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gamexco
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
